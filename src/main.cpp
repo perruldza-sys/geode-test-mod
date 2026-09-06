b7d8fa28dc211dd21d529c39716b116333f6d654
perruldza-sys <perruldza@gmail.com>
2026-09-06 15:14:42 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/PauseLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -17,7 +18,6 @@ static bool isNoclipEnabled = false;
 void toggleNoclip() {
     isNoclipEnabled = !isNoclipEnabled;
 
-    // Notifikasi
     if (isNoclipEnabled) {
         Notification::create("Noclip ON", NotificationIcon::Success)->show();
     } else {
@@ -28,38 +28,88 @@ void toggleNoclip() {
 }
 
 // =========================================================
-// HOOK MENU LAYER - TOMBOL NOCLIP
+// HELPER: bikin tombol Hoshino (dipakai ulang di MenuLayer & PauseLayer)
 // =========================================================
 
-class $modify(NoclipMenuLayer, MenuLayer) {
+CCMenuItemSpriteExtra* createHoshinoButton(CCObject* target, SEL_MenuHandler selector) {
+    auto sprite = CircleButtonSprite::create(
+        CCSprite::create("hoshino-button.png"_spr),
+        CircleBaseColor::Pink,
+        CircleBaseSize::Medium
+    );
+
+    return CCMenuItemSpriteExtra::create(sprite, target, selector);
+}
+
+// =========================================================
+// HOOK MENU LAYER - TOMBOL HOSHINO DI MENU UTAMA
+// =========================================================
+
+class $modify(TestButtonMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) {
             return false;
         }
 
-        // Tombol Noclip
-        auto sprite = CircleButtonSprite::create(
-            CCLabelBMFont::create("NC", "bigFont.fnt"),
-            CircleBaseColor::Cyan,
-            CircleBaseSize::Medium
-        );
-
-        auto myButton = CCMenuItemSpriteExtra::create(
-            sprite,
+        auto myButton = createHoshinoButton(
             this,
-            menu_selector(NoclipMenuLayer::onNoclipButton)
+            menu_selector(TestButtonMenuLayer::onTestButton)
         );
 
         if (auto bottomMenu = this->getChildByID("bottom-menu")) {
-            myButton->setID("noclip-button"_spr);
+            myButton->setID("test-button"_spr);
             bottomMenu->addChild(myButton);
             bottomMenu->updateLayout();
+        } else {
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+            auto fallbackMenu = CCMenu::create();
+            fallbackMenu->setID("test-button-menu"_spr);
+            fallbackMenu->addChild(myButton);
+            fallbackMenu->setPosition({winSize.width - 30.f, winSize.height - 30.f});
+            this->addChild(fallbackMenu);
         }
 
         return true;
     }
 
-    void onNoclipButton(CCObject* sender) {
+    void onTestButton(CCObject* sender) {
+        toggleNoclip();
+    }
+};
+
+// =========================================================
+// HOOK PAUSE LAYER - TOMBOL HOSHINO YANG SAMA DI MENU PAUSE
+// =========================================================
+
+class $modify(TestButtonPauseLayer, PauseLayer) {
+    bool init(bool p0) {
+        if (!PauseLayer::init(p0)) {
+            return false;
+        }
+
+        auto myButton = createHoshinoButton(
+            this,
+            menu_selector(TestButtonPauseLayer::onTestButton)
+        );
+
+        if (auto rightMenu = this->getChildByID("right-button-menu")) {
+            myButton->setID("test-button"_spr);
+            rightMenu->addChild(myButton);
+            rightMenu->updateLayout();
+        } else {
+            // Fallback: taruh manual kalau ID container-nya beda di versi ini
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+            auto fallbackMenu = CCMenu::create();
+            fallbackMenu->setID("test-button-menu"_spr);
+            fallbackMenu->addChild(myButton);
+            fallbackMenu->setPosition({winSize.width - 30.f, winSize.height - 30.f});
+            this->addChild(fallbackMenu);
+        }
+
+        return true;
+    }
+
+    void onTestButton(CCObject* sender) {
         toggleNoclip();
     }
 };
@@ -72,22 +122,19 @@ class $modify(NoclipPlayLayer, PlayLayer) {
     void onEnter() {
         PlayLayer::onEnter();
 
-        // Auto enable (opsional, butuh setting "noclip-on-start" di mod.json)
         if (Mod::get()->getSettingValue<bool>("noclip-on-start")) {
             if (!isNoclipEnabled) toggleNoclip();
         }
     }
 
     void keyDown(cocos2d::enumKeyCodes key, double dt) {
-    if (key == cocos2d::KEY_N) {
-        toggleNoclip();
-        return;
-    }
-    PlayLayer::keyDown(key, dt);
+        if (key == cocos2d::KEY_N) {
+            toggleNoclip();
+            return;
+        }
+        PlayLayer::keyDown(key, dt);
     }
 
-    // Ini fungsi yang beneran dipanggil pas player nabrak hazard.
-    // Kalau noclip aktif, kita skip pemanggilan aslinya -> nggak jadi mati.
     void destroyPlayer(PlayerObject* player, GameObject* object) {
         if (isNoclipEnabled) {
             log::info("Noclip prevented death!");
